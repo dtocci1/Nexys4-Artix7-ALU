ad4ad26f74826ee21f581c4e5f150ef48e3ec994
dtocci1 <dtocci1@umassd.edu>
2020-11-18 14:07:08 -0500

    updated program - old files should be removed

--- Nexys4-Artix7-ALU.srcs/sim_1/new/ALU_tb.sv
+++ Nexys4-Artix7-ALU.srcs/sim_1/new/ALU_tb.sv
@@ -1,28 +1,36 @@
 `begin_keywords "1800-2012" //Use keyword list from SystemVerilog-2012.
-program automatic test ( // This is the test bench. It specifies input signals to drive the DUT.
-    input               clk,
-    input  logic [7:0]  number1,number2,
-    input  logic [0:3]  opcode,
-    output logic [15:0] resultant
- );
+module ALU_tb;
 timeunit 1ns/1ns; // Simulation time unit of 1 ns, with precision of 1 ns.
 
-initial // Generate rstN.
+bit clk;
+reg[7:0]  number1,number2;
+reg[0:3]  opcode;
+reg[15:0] resultant;
+
+ALU ALU_tb(
+    .clk(clk),
+    .number1(number1),
+    .number2(number2),
+    .opcode(opcode),
+    .resultant(resultant)
+);
+
+// Generate clock
+always #50 clk = ~clk;
+
+initial
    begin
        number1 <= 8'h04;
        number2 <= 8'h02;
-   #20 opcode <= 4'b0000; // ADDITION. EXPECT 0X0007
+   #60 opcode <= 4'b0000; // ADDITION. EXPECT 0X0007
    assert(resultant == 16'h0006) else $error("Incorrect addition - OPC 0000");
-   #20 opcode <= 4'b0001; // SUBTRACTION EXPECT 0X0001
+   #110 opcode <= 4'b0001; // SUBTRACTION EXPECT 0X0001
    assert(resultant == 16'h0002) else $error("Incorrect subtraction - OPC 0001");
-   #20 opcode <= 4'b0010; // MULTIPLY EXPECT 0X0012
+   #160 opcode <= 4'b0010; // MULTIPLY EXPECT 0X0012
    assert(resultant == 16'h0008) else $error("Incorrect subtraction - OPC 0001");
-   #20 opcode <= 4'b0011; // DIVIDE EXPECT 0X0002
+   #210 opcode <= 4'b0011; // DIVIDE EXPECT 0X0002
    assert(resultant == 16'h0008) else $error("Incorrect subtraction - OPC 0001");
+   $finish;
    end
-
- initial   
-   #200 $finish;
- 
-endprogram: test
+endmodule
 `end_keywords
